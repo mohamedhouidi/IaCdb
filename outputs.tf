4b93e87b9987ec2a2e56c6c97e3193daa30c763a
mohamedhouidi <90357936+mohamedhouidi@users.noreply.github.com>
2024-01-15 14:49:12 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "dbServer_url" {
-  value = azurerm_mysql_server.dbserverQRQC.name
+  value = azurerm_mysql_server.dbserverbanking.name
 }
 
 output "dbServer_ips" {
-  value = azurerm_mysql_server.dbserverQRQC.fqdn
+  value = azurerm_mysql_server.dbserverbanking.fqdn
 }
 
 output "db_url" {
-  value = azurerm_mysql_database.QRQC.server_name
+  value = azurerm_mysql_database.Banking.server_name
 }
 
